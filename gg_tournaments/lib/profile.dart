277117bc58c99704d29656140dbb36663713ddda
busche-silver-genealogy <busche.silver.genealogy@gmail.com>
2020-01-18 00:49:43 -0800

    Added profile branch

---
+++ gg_tournaments/lib/profile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  Widget textWidget(String text) {
+    return Text(text);
+  }
+  
+  Widget rowWidget(String text1, String text2) {
+    return Row(
+      children: <Widget>[
+        Text(text1),
+        Text(text2)
+      ],
+    );
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        // Work from here
+        child: Column(
+          children: <Widget>[],
+        )
+      )
+    );
+  }
+}
