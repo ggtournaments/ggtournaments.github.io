a6f647614bf5ec0bffad06e88da9911bc0e3c204
Alan <amvasilk@ucsc.edu>
2020-01-18 20:58:10 -0800

    Finished the UI and info storage... Need DB

--- gg_tournaments/lib/eventInfo.dart
+++ gg_tournaments/lib/eventInfo.dart
@@ -7,30 +7,27 @@ class EventInfo extends StatefulWidget {
 }
 
 //Globals //////////////////////////////////////////////////////////////////////
-  String info;
-  String game;
-  String organizer;
-  String platform;
-  String type;
-  List<String> location = ["", "", "", "", ""];
+String info;
+String game;
+String organizer;
+String platform;
+String type;
+List<String> location = ["", "", "", "AK", ""];
 ////////////////////////////////////////////////////////////////////////////////
 
 BoxDecoration myBoxDecoration() {
   return BoxDecoration(
     border: Border.all(
-        width: 5.0,
+      width: 5.0,
       color: Colors.grey,
     ),
-    borderRadius: BorderRadius.all(
-        Radius.circular(5.0)
-    ),
+    borderRadius: BorderRadius.all(Radius.circular(5.0)),
     color: Colors.white,
   );
 }
 
 class _EventInfoState extends State<EventInfo> {
   @override
-
   Widget build(BuildContext context) {
     //Starts the display to the screen
     return Scaffold(
@@ -68,13 +65,14 @@ class _EventInfoState extends State<EventInfo> {
               padding: const EdgeInsets.all(8),
               child: TextField(
                 onChanged: (text) {
-                   info = text;
+                  info = text;
                 },
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
                 decoration: InputDecoration(
-                 focusedBorder: OutlineInputBorder(
-                    borderSide:BorderSide(color: Colors.greenAccent, width: 5.0),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.greenAccent, width: 5.0),
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.grey, width: 5.0),
@@ -89,30 +87,36 @@ class _EventInfoState extends State<EventInfo> {
             Container(
               padding: const EdgeInsets.all(8),
               child: Text('Game',
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.white,
-              )),
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                  )),
             ),
             Container(
-              padding: const EdgeInsets.all(8),
-              decoration: myBoxDecoration(),
-              child: DropdownButton<String>(
-                hint: Text('Game', textAlign: TextAlign.center),
-                value: game,
-                onChanged: (String newValue) {
-                  setState(() {
-                    game= newValue;
-                  });
-                },
-                items: <String>['Super Smash Brothers', 'League of Legends', 'PUBG', 'Overwatch', 'Vain Glory', 'CS:GO'].map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              )
-            ),
+                padding: const EdgeInsets.all(8),
+                decoration: myBoxDecoration(),
+                child: DropdownButton<String>(
+                  hint: Text('Game', textAlign: TextAlign.center),
+                  value: game,
+                  onChanged: (String newValue) {
+                    setState(() {
+                      game = newValue;
+                    });
+                  },
+                  items: <String>[
+                    'Super Smash Brothers',
+                    'League of Legends',
+                    'PUBG',
+                    'Overwatch',
+                    'Vain Glory',
+                    'CS:GO'
+                  ].map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                )),
             Container(
               padding: const EdgeInsets.all(8),
               child: Text('Organizer',
@@ -129,7 +133,8 @@ class _EventInfoState extends State<EventInfo> {
                 },
                 decoration: InputDecoration(
                   focusedBorder: OutlineInputBorder(
-                    borderSide:BorderSide(color: Colors.greenAccent, width: 5.0),
+                    borderSide:
+                        BorderSide(color: Colors.greenAccent, width: 5.0),
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.grey, width: 5.0),
@@ -157,18 +162,217 @@ class _EventInfoState extends State<EventInfo> {
                   value: platform,
                   onChanged: (String newValue) {
                     setState(() {
-                      platform= newValue;
+                      platform = newValue;
                     });
                   },
-                  items: <String>['Playstation 4', 'XBOX One', 'PC', 'Nintendo Switch', 'Nintendo Gamecube'].map((String value) {
+                  items: <String>[
+                    'Playstation 4',
+                    'XBOX One',
+                    'PC',
+                    'Nintendo Switch',
+                    'Nintendo Gamecube'
+                  ].map((String value) {
                     return DropdownMenuItem<String>(
                       value: value,
                       child: Text(value),
                     );
                   }).toList(),
-                )
+                )),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Text('Location',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                  )),
             ),
-            
+            Container(
+                padding: const EdgeInsets.all(8),
+                child: Row(children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: (text) {
+                        location[0] = text;
+                      },
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent, width: 5.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.grey, width: 5.0),
+                        ),
+                        labelText: 'Address 1:',
+                        hintText: 'Address 1',
+                        fillColor: Colors.white,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      onChanged: (text) {
+                        location[1] = text;
+                      },
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent, width: 5.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.grey, width: 5.0),
+                        ),
+                        labelText: 'Address 2:',
+                        hintText: 'Address 2',
+                        fillColor: Colors.white,
+                        filled: true,
+                      ),
+                    ),
+                  )
+                ])),
+            Container(
+                padding: const EdgeInsets.all(8),
+                child: Row(children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: (text) {
+                        location[2] = text;
+                      },
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent, width: 5.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.grey, width: 5.0),
+                        ),
+                        labelText: 'City:',
+                        hintText: 'City',
+                        fillColor: Colors.white,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                        decoration: myBoxDecoration(),
+                        margin: EdgeInsets.all(20),
+                        child: DropdownButton<String>(
+                          hint: Text('State', textAlign: TextAlign.center),
+                          value: location[3],
+                          onChanged: (String newValue) {
+                            setState(() {
+                              location[3] = newValue;
+                            });
+                          },
+                          items: <String>[
+                            'AK',
+                            'AL',
+                            'AR',
+                            'AZ',
+                            'CA',
+                            'CO',
+                            'CT',
+                            'DE',
+                            'FL',
+                            'GA',
+                            'HI',
+                            'IA',
+                            'ID',
+                            'IL',
+                            'IN',
+                            'KS',
+                            'KY',
+                            'LA',
+                            'MA',
+                            'MD',
+                            'ME',
+                            'MI',
+                            'MN',
+                            'MO',
+                            'MS',
+                            'MT',
+                            'NC',
+                            'ND',
+                            'NE',
+                            'NH',
+                            'NJ',
+                            'NM',
+                            'NV',
+                            'NY',
+                            'OH',
+                            'OK',
+                            'OR',
+                            'PA',
+                            'RI',
+                            'SC',
+                            'SD',
+                            'TN',
+                            'TX',
+                            'UT',
+                            'VA',
+                            'VT',
+                            'WA',
+                            'WI',
+                            'WV',
+                            'WY'
+                          ].map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        )),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      onChanged: (text) {
+                        location[4] = text;
+                      },
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent, width: 5.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.grey, width: 5.0),
+                        ),
+                        labelText: 'Zip code:',
+                        hintText: 'Zip',
+                        fillColor: Colors.white,
+                        filled: true,
+                      ),
+                    ),
+                  )
+                ]
+              ),
+            ),
+            Container(
+              child: Center(
+                child: FlatButton(
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.brown)),
+                  color: Colors.white,
+                  textColor: Colors.brown,
+                  padding: EdgeInsets.all(8.0),
+                  onPressed: () {},
+                  child: Text(
+                    "Next".toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ),
+              )
+            )
           ],
         ),
       ),
